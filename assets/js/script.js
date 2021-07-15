e4cf1b6d573257fe5c34e8b7b9e91003cd796300
jerismith32 <jerismith32@gmail.com>
2021-07-15 03:47:31 -0400

    updated all the feature folders and the index.html page with testing images

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+//Initializes the mobile navbar
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+});
+
+// Initialization of Carousel
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.carousel');
+//     var instances = M.Carousel.init(elems, options);
+//   });
+
+//   // Or with jQuery
+
+//   $(document).ready(function(){
+//     $('.carousel').carousel();
+//   });
